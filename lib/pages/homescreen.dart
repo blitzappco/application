01bc19f0d8306d03b57b81abbd470bc29ff320fc
Gabriel Stanciu <57194563+ThePizzaCode@users.noreply.github.com>
2024-05-12 16:06:21 +0300

    Added location getter and map camera animations

--- lib/pages/homescreen.dart
+++ lib/pages/homescreen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:application/components/modals/main_modal.dart';
+import 'package:application/maps/map_controller.dart';
+import 'package:application/utils/get_location.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -10,6 +14,22 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+  late GoogleMapController mapController;
+
+  void setupPositionLocator() async {
+    final pos = await getCurrentLocation();
+
+    CameraPosition cp = CameraPosition(
+        target: LatLng(
+          pos?.latitude ?? 0.0,
+          pos?.longitude ?? 0.0,
+        ),
+        zoom: 17);
+    mapController.animateCamera(CameraUpdate.newCameraPosition(cp));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +47,11 @@ class _HomescreenState extends State<Homescreen> {
               target: LatLng(44.4364384, 26.0863522),
               zoom: 14.0,
             ),
+            onMapCreated: (GoogleMapController controller) async {
+              _controller.complete(controller);
+              mapController = controller;
+              setCameraLocation(mapController);
+            },
           ),
           //Bottom screen
           MainModal(),
