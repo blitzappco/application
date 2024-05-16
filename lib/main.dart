60f03de4ae0ba1e248f4e653441ecd9c055c5064
Soare Catalin <stevensuncatalin369@gmail.com>
2024-05-16 22:40:01 +0300

    worked on models and google maps functions. have to write the providers and some other utilities.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:application/pages/homescreen.dart';
+import 'package:application/pages/maps_test.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Homescreen(),
+    return const MaterialApp(
+      home: MapsTest(),
     );
   }
 }
