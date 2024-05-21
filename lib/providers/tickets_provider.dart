7169dabd76e7b3d806974292cb4f11bf83139e3a
Soare Catalin <stevensuncatalin369@gmail.com>
2024-05-21 22:21:53 +0300

    payments and purchase functions for providers

--- lib/providers/tickets_provider.dart
+++ lib/providers/tickets_provider.dart
@@ -9,11 +9,17 @@ import '../models/ticket.dart';
 class TicketsProvider with ChangeNotifier {
   List<Ticket> list = [];
 
+  Ticket purchased = Ticket();
+
   late Ticket last;
   late bool show;
 
   bool loading = false;
   String errorMessage = '';
+
+  double fare = 0.0;
+  String ticketID = '';
+  bool confirmed = true;
 
   // get ticket types per city
   getTicketTypes(String token, String city) async {
@@ -90,6 +96,85 @@ class TicketsProvider with ChangeNotifier {
       notifyListeners();
     } else {
       errorMessage = 'Nu s-au putut gasi biletele existente';
+      notifyListeners();
+    }
+  }
+
+  createPurchaseIntent(String token, String typeID) async {
+    loading = true;
+    notifyListeners();
+
+    final response = await http.post(
+        Uri.parse('${AppURL.baseURL}/accounts/tickets/purchase-intent'
+            '?typeID=$typeID'),
+        headers: authHeader(token));
+
+    loading = false;
+    notifyListeners();
+
+    final json = jsonDecode(utf8.decode(response.bodyBytes));
+    if (response.statusCode == 200) {
+      ticketID = json['ticketID'];
+      fare = json['fare'];
+      notifyListeners();
+    } else {
+      errorMessage = json['message'];
+      notifyListeners();
+    }
+  }
+
+  attachPurchasePayment(String token, String paymentIntent) async {
+    loading = true;
+    notifyListeners();
+
+    final response = await http.post(
+        Uri.parse('${AppURL.baseURL}/accounts/tickets/purchase-intent'),
+        headers: authHeader(token),
+        body: jsonEncode(<String, String>{
+          "paymentIntent": paymentIntent,
+          "ticketID": ticketID,
+        }));
+    loading = false;
+    notifyListeners();
+
+    final json = jsonDecode(utf8.decode(response.bodyBytes));
+    if (response.statusCode == 200) {
+      purchased = Ticket.fromJSON(json);
+      confirmed = false;
+      notifyListeners();
+    } else {
+      errorMessage = json['message'];
+      notifyListeners();
+    }
+  }
+
+  confirmPurchase(String token) async {
+    loading = true;
+    notifyListeners();
+
+    final response = await http.get(
+      Uri.parse('${AppURL.baseURL}/accounts/tickets/ticket'
+          '?ticketID=$ticketID'),
+      headers: authHeader(token),
+    );
+
+    loading = false;
+    notifyListeners();
+
+    final json = jsonDecode(utf8.decode(response.bodyBytes));
+    if (response.statusCode == 200) {
+      final tempTicket = Ticket.fromJSON(json);
+      if (tempTicket.confirmed == true) {
+        purchased = Ticket();
+        ticketID = '';
+        fare = 0.0;
+        last = tempTicket;
+        list.add(tempTicket);
+        confirmed = true;
+      }
+      notifyListeners();
+    } else {
+      errorMessage = json['message'];
       notifyListeners();
     }
   }
