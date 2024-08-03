031f5ce372e7321469347d9ba68512dbcf665eda
Gabriel Stanciu <57194563+ThePizzaCode@users.noreply.github.com>
2024-08-03 15:51:29 +0300

    Fixed post clone ci

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -16,6 +16,8 @@ flutter precache --ios
 # Install Flutter dependencies.
 flutter pub get
 
+echo "MAPS_KEY=${MAPS_KEY}" > .env
+
 # Install CocoaPods using Homebrew.
 HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install cocoapods
